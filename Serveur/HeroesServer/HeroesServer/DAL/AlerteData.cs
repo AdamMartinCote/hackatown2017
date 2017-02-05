8d2f60842ce5ae82d92b85ef7b335156f7762ef4
Christophe Carreau-Lacasse <chris.carreau@hotmail.com>
2017-02-05 05:09:21 -0500

    plein de shit

--- Serveur/HeroesServer/HeroesServer/DAL/AlerteData.cs
+++ Serveur/HeroesServer/HeroesServer/DAL/AlerteData.cs
@@ -3,11 +3,60 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
 
 namespace HeroesServer.DAL
 {
     public static class AlerteData
     {
+        public static List<Alerte> GetAllAlerteEnCours()
+        {
+
+            List<Alerte> alertes = new List<Alerte>();
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = "Data Source = mssql3.gear.host; Initial Catalog = heroes; Persist Security Info = True; User ID = heroes; Password = hackatown_heroes";
+                conn.Open();
+
+                // Create the command
+                SqlCommand command = new SqlCommand(@"   SELECT 
+                                                         a.IdAlerte
+                                                        , a.IdInitiateur
+                                                        , a.IdRepondant
+                                                        , a.Gravite
+                                                        , a.Type
+                                                        , a.IsAnswered
+                                                        , p.Latitude
+                                                        , p.Longitude 
+                                                        FROM Alerte a 
+                                                        JOIN Position p 
+                                                        ON p.IdAlerte = a.IdAlerte 
+                                                        WHERE IsAnswered = 0", conn);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    // while there is another record present
+                    while (reader.Read())
+                    {
+                        Alerte alerte = new Alerte();
+                        alerte.IdAlert = int.Parse(reader[0].ToString());
+                        alerte.IdInitiateur = int.Parse(reader[1].ToString());
+                        alerte.IdRepondant = reader[2] == DBNull.Value ? 0 : int.Parse(reader[2].ToString());
+                        alerte.Gravite = (Gravite)int.Parse(reader[3].ToString());
+                        alerte.Type = (TypeAlerte)int.Parse(reader[4].ToString());
+                        alerte.IsAnswered = reader[5].ToString() == "1";
+                        Localisation loc = new Localisation();
+                        loc.Latitude = float.Parse(reader[6].ToString());
+                        loc.Longitude = float.Parse(reader[7].ToString());
+                        alerte.Position = loc;
+                        alertes.Add(alerte);
+                    }
+                }
+            }
+            
+            return alertes;
+        }
+
         public static List<Alerte> Get()
         {
             return null;
--- Serveur/HeroesServer/HeroesServer/DAL/UserData.cs
+++ Serveur/HeroesServer/HeroesServer/DAL/UserData.cs
@@ -3,19 +3,58 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
 
 namespace HeroesServer.DAL
 {
     public class UserData
     {
-        public static List<Users> Get()
+        public static List<User> Get()
         {
             return null;
         }
 
         public static User Get(int id)
         {
-            return null;
+            User user = new User();
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = "Data Source = mssql3.gear.host; Initial Catalog = heroes; Persist Security Info = True; User ID = heroes; Password = hackatown_heroes";
+                conn.Open();
+
+                // Create the command
+                SqlCommand command = new SqlCommand("SELECT  u.[IdUser] "+
+                                                          ",[Nom] " +
+                                                          ",[Prenom] " +
+                                                          ",[NiveauCompetence] " +
+	                                                      ",p.Latitude " +
+	                                                      ",p.Longitude " +
+	                                                      ",p.LastUpdate " +
+                                                      "FROM [heroes].[dbo].[User] u " +
+                                                      "JOIN [dbo].[Position] p " +
+                                                      "ON p.IdUser = u.IdUser " +
+                                                      "WHERE u.IdUser = @idUser ", conn);
+                // Add the parameters.
+                command.Parameters.Add(new SqlParameter("idUser", id));
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    // while there is another record present
+                    while (reader.Read())
+                    {
+                        user.idUser = int.Parse(reader[0].ToString());
+                        user.Nom = reader[1].ToString();
+                        user.Prenom = reader[2].ToString();
+                        user.NiveauCompetence = (NiveauCompetence) int.Parse(reader[3].ToString());
+                        Localisation loc = new Localisation();
+                        loc.Latitude = float.Parse(reader[4].ToString());
+                        loc.Longitude = float.Parse(reader[5].ToString());
+                        loc.LastUpdate = DateTime.Parse(reader[6].ToString());
+                        user.Position = loc;
+                    }
+                }
+            }
+            return user;
         }
 
         public static User Insert(User user)
@@ -23,6 +62,27 @@ namespace HeroesServer.DAL
             return null;
         }
 
+        public static User UpdatePositionUser(int id, float longitude, float latitude)
+        {
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = "Data Source = mssql3.gear.host; Initial Catalog = heroes; Persist Security Info = True; User ID = heroes; Password = hackatown_heroes";
+                conn.Open();
+
+                // Create the command
+                SqlCommand command = new SqlCommand("UPDATE Position " +
+                                                      "SET   LastUpdate = GETDATE(), " +
+                                                            "Latitude = @lat, " +
+                                                            "Longitude = @long " +
+                                                      "WHERE IdUser = @idUser", conn);
+                // Add the parameters.
+                command.Parameters.Add(new SqlParameter("idUser", id));
+                command.Parameters.Add(new SqlParameter("lat", latitude));
+                command.Parameters.Add(new SqlParameter("long", longitude));
+                command.ExecuteNonQuery();
+            }
+            return null;
+        }
 
         public static User Update(int id)
         {
