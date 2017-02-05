7ec4333d3844b756ffdbc1abfbbac10c9be1ea99
Christophe Carreau-Lacasse <chris.carreau@hotmail.com>
2017-02-04 23:57:09 -0500

    Server

---
+++ Serveur/HeroesServer/HeroesServer/DAL/AlerteData.cs
@@ -0,0 +1,38 @@
+﻿using HeroesServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HeroesServer.DAL
+{
+    public class AlerteData
+    {
+        public List<Alerte> Get()
+        {
+            return null;
+        }
+
+        public Alerte Get(int id)
+        {
+            return null;
+        }
+
+        public Alerte Insert(Alerte alerte)
+        {
+            return null;
+        }
+
+
+        public Alerte Update(int id)
+        {
+            return null;
+        }
+
+
+        public Alerte Delete(int id)
+        {
+            return null;
+        }
+    }
+}
--- Serveur/HeroesServer/HeroesServer/Models/Alerte.cs
+++ Serveur/HeroesServer/HeroesServer/Models/Alerte.cs
@@ -5,13 +5,16 @@ using System.Web;
 
 namespace HeroesServer.Models
 {
-    public enum Gravite { BAS, ELEVE}
+    public enum Gravite { BAS=0, MOYEN ,ELEVE}
+    public enum TypeAlerte { MEDICAL=0, SECURITE }
     public class Alerte
     {
-        public ApplicationUser Initiateur { get; set; }
-        public ApplicationUser Repondant { get; set; }
+        public int IdAlert { get; set; }
+        public int IdInitiateur { get; set; }
+        public int IdRepondant { get; set; }
         public Localisation Position { get; set; }
         public Gravite Gravite { get; set; }
+        public TypeAlerte Type { get; set; }
 
     }
 }
