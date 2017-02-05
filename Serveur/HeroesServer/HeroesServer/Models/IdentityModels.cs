a03caa1e052d4c1f35e7ecda1a601808a494e68d
marchounto <mghounto@gmail.com>
2017-02-04 23:46:09 -0800

    serveurs

--- Serveur/HeroesServer/HeroesServer/Models/IdentityModels.cs
+++ Serveur/HeroesServer/HeroesServer/Models/IdentityModels.cs
@@ -9,7 +9,6 @@ namespace HeroesServer.Models
     // Faible = aucune connaissance en santé
     // Moyen = RCR ou équivalent
     // Expert = Infirmier/medecin/whatever
-    public enum NiveauCompetence{FAIBLE, MOYEN, EXPERT };
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
@@ -18,11 +17,11 @@ namespace HeroesServer.Models
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
             // Add custom user claims here
-            userIdentity.AddClaim(new Claim("NiveauCompetence", this.NiveauCompetence.ToString()));
+            //userIdentity.AddClaim(new Claim("NiveauCompetence", this.NiveauCompetence.ToString()));
 
             return userIdentity;
         }
-        public NiveauCompetence? NiveauCompetence { get; set; }
+        //public NiveauCompetence? NiveauCompetence { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
